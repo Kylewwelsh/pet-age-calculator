78c740cdbdda7ad53f0a6f0eac58964f54da6cb8
kylewwelsh <kyle@kdmdental.com>
2021-11-03 20:03:00 -0600

    cat calculation added to script

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,25 @@ dogCalculation.addEventListener (
     let currentDogAge = document.querySelector('.current-dog-age').value;
     let dogAge = currentDogAge * 7;
 
-    document.querySelector('.dog-calculation-text').innerHTML = 'As of today, ' + dogName + ' is ' + dogAge + ' years old in human years!';  
+    document.querySelector('.dog-calculation-text').innerHTML = 'As of today, ' + dogName + ' is ' + dogAge + ' in human years!';  
   }
-)
+);
+
+// Cat Calculator
+const catCalculation = document.querySelector('.cat-submit');
+
+catCalculation.addEventListener (
+  'click', () => {
+    let catName = document.querySelector('.cat-name').value;
+    let currentCatAge = document.querySelector('.current-cat-age').value;
+    let catAge;
+
+    if (currentCatAge > 1) {
+      catAge = (15 + 9 + (currentCatAge - 2) * 4);
+    } else {
+      catAge = 15;
+    }
+
+    document.querySelector('.cat-calculation-text').innerHTML = 'As of today, ' + catName + ' is ' + catAge + ' in human years!'; 
+  }
+);
