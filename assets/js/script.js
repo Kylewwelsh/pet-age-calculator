92020d539e05d4a9deaace449a807d41cc8f27d1
kylewwelsh <kyle@kdmdental.com>
2021-11-03 20:39:51 -0600

    bunny calculation added to script, had trouble finding a calculation so manually inputed 10 years

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,3 +29,60 @@ catCalculation.addEventListener (
     document.querySelector('.cat-calculation-text').innerHTML = 'As of today, ' + catName + ' is ' + catAge + ' in human years!'; 
   }
 );
+
+// bunny Calculator - unable to find concrete mathematical formula, manually inputting for 1-10 year old bunnies off a vet chart
+const bunnyCalculation = document.querySelector('.bunny-submit');
+
+bunnyCalculation.addEventListener (
+  'click', () => {
+    let bunnyName = document.querySelector('.bunny-name').value;
+    let currentBunnyAge = document.querySelector('.current-bunny-age').value;
+    let bunnyAge;
+
+    if (currentBunnyAge == 1) {
+      bunnyAge = 21
+    }
+
+    else if (currentBunnyAge == 2) {
+      bunnyAge = 27
+    }
+
+    else if (currentBunnyAge == 3) {
+      bunnyAge = 33
+    }
+
+    else if (currentBunnyAge == 4) {
+      bunnyAge = 39
+    }
+
+    else if (currentBunnyAge == 5) {
+      bunnyAge = 45
+    }
+
+    else if (currentBunnyAge == 6) {
+      bunnyAge = 51
+    }
+
+    else if (currentBunnyAge == 7) {
+      bunnyAge = 57
+    }
+
+    else if (currentBunnyAge == 8) {
+      bunnyAge = 63
+    }
+
+    else if (currentBunnyAge == 9) {
+      bunnyAge = 69
+    }
+
+    else if (currentBunnyAge == 10) {
+      bunnyAge = 75
+    }
+
+    else {
+      bunnyAge = '-'
+    }
+
+    document.querySelector('.bunny-calculation-text').innerHTML = 'As of today, ' + bunnyName + ' is ' + bunnyAge + ' in human years!'; 
+  }
+);
